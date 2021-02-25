7c4fc68384a7695d60aa90dd274c07e5c188d8c0
Janae Quinones <janaemassa@gmail.com>
2021-02-24 23:08:09 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+}
+// With the .push() method, we can add elements to the end of an Array - Destructive
+// We can also .unshift() elements onto the beginning of an Array - Destructive
+
+function destructivelyRemoveLastDriver(name) {
+    drivers.pop(name);
+}
+
+function destructivelyRemoveFirstDriver(name) {
+    drivers.shift(name);
+}
+// The .pop() method removes the last element in an Array - Destructive
+// The .shift() method removes the first element in an Array - Destructive
+
+function appendDriver(name) {
+    return [...drivers, name];
+} 
+
+function prependDriver(name) {
+    return [name, ...drivers];
+} 
+
+function removeLastDriver() {
+    return drivers.slice(0, drivers.length - 1);
+} 
+
+function removeFirstDriver() {
+    return drivers.slice(1)
+} 
+
+// combining .slice() and the spread operator allows us to replace elements nodestructively
